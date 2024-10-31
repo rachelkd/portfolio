5a71f99f3432bf30580bb66b847fb4daab7ea1fe
rachelkd <rachel05deng@gmail.com>
2024-10-31 00:35:41 -0700

    Removed unused imports

--- src/components/JobsList.tsx
+++ src/components/JobsList.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import { styled, useTheme } from '@mui/material/styles';
 
 interface ExperienceItem {
